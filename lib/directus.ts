66aa1cbe54e92799bdb97646bda6dce875b51c89
Aleks Rūtiņš <aleks@rutins.com>
2024-04-25 12:11:49 -0400

    I may have made an oops

--- lib/directus.ts
+++ lib/directus.ts
@@ -4,4 +4,4 @@ import type { Schema } from './directus-types';
 export type Article = Schema['articles'][number];
 export type Issue = Schema['issues'][number];
 export type Column = Schema['columns'][number];
-export default createDirectus<Schema>(process.env.DIRECTUS_URL ?? 'https://bulletin-cms.burrburton.org').with(rest());
+export default createDirectus<Schema>(process.env.DIRECTUS_URL ?? 'https://bulletin-cms.burrburton.org/').with(rest());
